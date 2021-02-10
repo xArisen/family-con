c97f13f8a4090cd40a6c4f369f053b0a037ea9bb
xArisen <czapiewskipiotr04@gmail.com>
2021-02-10 15:06:02 +0100

    Initial backend commit

---
+++ Backend/FamilyCon/src/main/java/com/xArisen/FamilyCon/FamilyConApplication.java
@@ -0,0 +1,13 @@
+package com.xArisen.FamilyCon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyConApplication.class, args);
+	}
+
+}
