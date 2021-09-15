dcfc7f0a72ac48875e214ca3ce51121bbd838c48
xArisen <czapiewskipiotr04@gmail.com>
2021-09-15 21:02:47 +0200

    Dodaj pobieranie i wysylanie kalenadzry

---
+++ Backend/FamilyCon/src/main/java/com/xArisen/FamilyCon/dto/CalendarDropdownDto.java
@@ -0,0 +1,16 @@
+package com.xArisen.FamilyCon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalendarDropdownDto {
+    private Long id;
+    private String name;
+    private String description;
+}
--- Frontend/webapp/src/components/CalendarGetter.jsx
+++ Frontend/webapp/src/components/CalendarGetter.jsx
@@ -2,40 +2,60 @@ import React, { useState, useEffect } from 'react'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import '../style/CalendarGetter.css'
-import axios from 'axios';
-import {getToken} from '../UseToken';
+import {getToken, removeToken} from '../UseToken';
 
 
 
-const getAllUserCallendars = async () => {
-  return axios.get('http://localhost:8080/calendar', {
-      headers: {
-        'Authorization': `Bearer ${getToken()}`
-      }
-    }).then(response => callendars = response.data);
-}
 
-let callendars = await getAllUserCallendars();
-callendars = callendars +1;
-function CalendarGetter() {
-    const [dropDownDisabled, setDropDownDisabled] = useState(true);
+class CalendarGetter extends React.Component {
+  state = {
+    loading: true,
+    allCalendars: []
+  };
 
-    useEffect(() => {
-        if(callendars == []){
-          getAllUserCallendars();
+  getAllCalendarsData = async () => {
+    const url = 'http://localhost:8080/calendar';
+    const response = await fetch(url, { 
+        method: 'GET', 
+        headers: new Headers({
+          'Authorization': `Bearer ${getToken()}`
+        })});
+    if(response.status == 403){
+      removeToken();
+    }
+    const data = await response.json();
+    this.setState({allCalendars: data})
+    console.log(data);
+  }
+
+  createCalendar = async () => {
+    const url = 'http://localhost:8080/calendar';
+    const response = await fetch(url, { 
+        method: 'POST', 
+        body: JSON.stringify({
+          name: "praca"
+        }),
+        headers: new Headers({
+          'Authorization': `Bearer ${getToken()}`,
+          "Content-type": "application/json; charset=UTF-8"
+        })});
+        
+        if(response.status == 403){
+          removeToken();
         }
-        callendars == [] ? setDropDownDisabled(true) : setDropDownDisabled(false);
-        console.log(callendars);
-        console.log(dropDownDisabled);
-    }, [getAllUserCallendars])
+    const data = await response.json();
+    console.log(data);
+  }
 
-    return (
+    render() { 
+      return (
         <div className="custom-calendargetter-container">
             <h3>Wybierz kalendarz:</h3>
-            <Dropdown className="custom-dropdown" options={callendars} disabled={dropDownDisabled} placeholder="Wybierz..." />
-            <input type="button" value="Click me!" onClick={() => getAllUserCallendars()} />
+            <Dropdown className="custom-dropdown" options={this.state.allCalendars.map(calendar => calendar.name)} placeholder="Wybierz..." />
+            <input type="button" value="Get calendars" onClick={this.getAllCalendarsData} />
+            <input type="button" value="Add calendar" onClick={this.createCalendar} />
         </div>
-    )
+      )}
 }
 
 export default CalendarGetter
